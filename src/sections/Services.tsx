297c0e6f646043440fb506f3e237dd0d108a2762
Sants <sants@Santss-MacBook-Pro.local>
2026-01-18 12:12:29 -0700

    fix(services): Services updated font sizes

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -49,11 +49,11 @@ const Services = () =>{
                         <div className="flex items-center justify-between gap-4 font-light">
                             <div className="flex flex-col gap-6">
                                 <h2 className="text-4xl lg:text-txl">{service.title}</h2>
-                                <p className="text-xl leading-relaxed tracking-widest lg:text-2xl text-white/60 text-pretty">{service.description}</p>
+                                <p className="text-base leading-relaxed tracking-widest lg:text-2xl text-white/60 text-pretty">{service.description}</p>
                                 <div className="flex flex-col gap-2 text-2xl sm:gap-4 lg:text-3xl text-white/80">
                                 {service.items?.map((item:any , itemIndex: number) => (
                                     <div key={`item-${index}-${itemIndex}`}>
-                                        <h3 className="flex">
+                                        <h3 className="flex text-base">
                                             <span className="mr-12 text-lg text-white/30 ">0{itemIndex + 1}</span>
                                             {item.title}
                                         </h3>
